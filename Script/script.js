9688e325b729efc39db5a1ec0001c115f6ed38e8
danielkhavin <115034985+Dannykhvn@users.noreply.github.com>
2022-11-11 17:09:35 -0800

    adjusted code -- ignore commit

--- Script/script.js
+++ Script/script.js
@@ -28,23 +28,3 @@ if (check === false){
     courselist.push({code: `ACIT ${input}`, coursename: null})
     console.log(`Object '${input}' added to courselist`)
 }
-
-// let input = null;
-
-// do{
-//     input = prompt("Enter a 4 digit number!")}
-//     while(isNaN(input) == true || input.toString().length > 4 || input.toString().length < 4);
-
-
-// let check = false;
-// for (let mycourses of courselist){
-//     if (mycourses.code.includes(input)){
-//     console.log(`Yes, I am taking the course: ACIT - ${mycourses.code}, ${mycourses.coursename}`)
-//     check = true;
-//     }
-// }
-
-// if (check === false){
-//     courselist.push({code: `${input}`, coursename: null})
-//     console.log(`Object '${input}' added to courselist`)
-// }
